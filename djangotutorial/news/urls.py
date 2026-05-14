ddbf65b6734c72fa72f7378d38690a6f9846c910
Thanhtu1824 <thanhtua20s@gmail.com>
2026-05-14 18:01:05 +0700

    xem danh muc bai viet

--- djangotutorial/news/urls.py
+++ djangotutorial/news/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("tinymce/", include("tinymce.urls")),
     path("article/<slug:slug>/", views.article_detail, name="article_detail"),
+    path("category/<slug:slug>/", views.category_detail, name="category_detail"),
+    path("subcategory/<slug:slug>/", views.subcategory_detail, name="subcategory_detail"),
 ]
--- djangotutorial/news/views.py
+++ djangotutorial/news/views.py
@@ -15,6 +15,46 @@ def index(request):
 
 def article_detail(request, slug):
     article = get_object_or_404(Article, slug=slug)
-    return render(request, "news/detail.html", {"article": article})
+    categories = Category.objects.all()
+    subcategories = SubCategory.objects.all()
 
+    related_articles = Article.objects.filter(
+        SubCategory=article.SubCategory
+    ).exclude(
+        id=article.id
+    )
 
+    return render(request, "news/detail.html", {
+        "article": article,
+        "categories": categories,
+        "subcategories": subcategories,
+        "related_articles": related_articles,
+    })
+
+def category_detail(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+
+    categories = Category.objects.all()
+    subcategories = SubCategory.objects.filter(category=category)
+
+    articles = Article.objects.filter(Category=category)
+
+    return render(request, "news/category_detail.html", {
+        "category": category,
+        "categories": categories,
+        "subcategories": subcategories,
+        "articles": articles,
+    })
+
+def subcategory_detail(request, slug):
+    subcategory = get_object_or_404(SubCategory, slug=slug)
+    categories = Category.objects.all()
+    subcategories = SubCategory.objects.all()
+    articles = Article.objects.filter(SubCategory=subcategory)
+
+    return render(request, "news/subcategory_detail.html", {
+        "subcategory": subcategory,
+        "categories": categories,
+        "subcategories": subcategories,
+        "articles": articles,
+    })
